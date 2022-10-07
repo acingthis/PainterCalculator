735456f7a980fc5db00e486ce7c70cf9b4e4060f
Robert <acing-psn@outlook.com>
2022-10-07 12:24:44 +0100

    Creation of painter calculator

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args)
+    {
+        double CanSize = 20;
+
+        double RoomHeight = 8.2;
+        double RoomWidth = 17.9;
+        double RoomLength = 7.7;
+
+        double PaintUsed = (RoomHeight * RoomWidth) * 2;
+        PaintUsed = PaintUsed + (RoomHeight * RoomLength) * 2;
+
+        System.out.println("The amount of paint used is: " + PaintUsed + "m");
+
+        int
+
+        System.out.println("The amount of can used is: " );
+
+
+    }
+}
