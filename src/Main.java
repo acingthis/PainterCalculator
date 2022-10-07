4b34ead1ca726f942514438156b432679d0e58f0
Robert <acing-psn@outlook.com>
2022-10-07 16:34:24 +0100

    Added more user input

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 class Main {
@@ -6,36 +5,67 @@ class Main {
     {
         double CanSize = 20;
 
-        double RoomHeight = 8.2;
-        double RoomWidth = 17.9;
-        double RoomLength = 7.7;
+        double RoomHeight;
+        double RoomWidth;
+        double RoomLength;
+
+        //Scanner
+        Scanner Scan = new Scanner(System.in);
+
+        do {
+            System.out.println("what is the height of the room?");
+            System.out.print("> ");
+            RoomHeight = Double.parseDouble(Scan.nextLine());
+        }
+        while (RoomHeight == 0);
+        do {
+            System.out.println("what is the width of the room?");
+            System.out.print("> ");
+            RoomWidth = Double.parseDouble(Scan.nextLine());
+        }
+        while (RoomWidth == 0);
+        do {
+            System.out.println("what is the length of the room?");
+            System.out.print("> ");
+            RoomLength = Double.parseDouble(Scan.nextLine());
+        }
+        while (RoomLength == 0);
+
+        //Calculations
 
         double PaintUsed = (RoomHeight * RoomWidth) * 2;
         PaintUsed = PaintUsed + (RoomHeight * RoomLength) * 2;
-
         System.out.println("The amount of paint used is: " + PaintUsed + "m");
 
         int CansUsed = (int) (PaintUsed / CanSize) + 1;
-
         System.out.println("The amount of cans used is: " + CansUsed + " cans");
 
 
         //type of paint and cost
 
         String TypeOfPaint = "";
-        //Scanner
-        Scanner Scan = new Scanner(System.in);
         System.out.println("What kind of paint do you want to use?");
         System.out.print("> ");
         TypeOfPaint = Scan.nextLine();
 
         double CostOfPaint = switch (TypeOfPaint) {
+            case "Best" -> 1000;
             case "primer","Primer" -> 20.99;
             case "emulsion","Emulsion" -> 10.45;
-            case "oil","Oil" -> 30.25;
+            case "oil","Oil" -> 40.25;
             default -> 25;
         };
         double CostOfPainting = CostOfPaint * CansUsed;
+
+        if (CostOfPaint == 25)
+        {
+            System.out.println("The paint type was unknown so the average was used £25f");
+        }
+
+        for (int x = 0; x < 5;x++)
+        {
+            System.out.println("MONEY");
+        }
 
         System.out.println("The Cost of Paint is: £" + CostOfPainting);
     }
